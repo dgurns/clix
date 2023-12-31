10a4e99ba195d7c14acb5868ddbf6f9421f71c19
Dan Gurney <dan@dangurney.net>
2023-12-31 23:36:34 +0000

    feat: add user input

--- cmd/clix/main.go
+++ cmd/clix/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/dgurns/clix/internal/session"
 )
 
 func main() {
-	fmt.Println("Hello")
+	s := &session.Session{}
+
+	err := s.Advance(&session.Message{
+		Role: session.RoleSystem,
+		Content: `Welcome to Clix! I can help you run commands on your computer. What would you like to do? 
+For example, "Reorganize my desktop" or "Initialize a new git repository"`,
+	})
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
 }
--- internal/session/session.go
+++ internal/session/session.go
@@ -1,13 +1,78 @@
 package session
 
-type State int
-
-const (
-	StateSystemAsking State = iota
-	StateUserAnswering
-	StateToolUsing
+import (
+	"bufio"
+	"fmt"
+	"os"
 )
 
+type Role string
+
+const (
+	RoleSystem Role = "system"
+	RoleUser   Role = "user"
+	RoleTool   Role = "tool"
+)
+
+type Message struct {
+	Role    Role
+	Content string
+}
+
 type Session struct {
-	State State
+	Messages []*Message
+}
+
+func promptUser() (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("> ")
+	s, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+	fmt.Println()
+	return s[:len(s)-1], nil
+}
+
+func (s *Session) Advance(msg *Message) error {
+	s.Messages = append(s.Messages, msg)
+	switch msg.Role {
+	case RoleSystem:
+		fmt.Printf("🤖 %s\n\n", msg.Content)
+
+		u, err := promptUser()
+		if err != nil {
+			return err
+		}
+		if u == "" {
+			return fmt.Errorf("no user input")
+		}
+
+		err = s.Advance(&Message{
+			Role:    RoleUser,
+			Content: u,
+		})
+		if err != nil {
+			return err
+		}
+	case RoleUser:
+		fmt.Print("Querying LLM...\n\n")
+		// TODO: Get a chat completion from the LLM
+		fmt.Print("I suggest you run: `ls`\n\n")
+		fmt.Print("Want to run it? (y)es / (n)o\n\n")
+		// TODO: Add a user prompt for yes/no
+		fmt.Print("> y\n\n")
+		err := s.Advance(&Message{
+			Role:    RoleTool,
+			Content: "ls",
+		})
+		if err != nil {
+			return err
+		}
+	case RoleTool:
+		fmt.Print("Running `ls`...\n\n")
+		fmt.Print("Output:\n\nDesktop\nDownloads\nDocuments\n\n")
+		// TODO: Call Advance with RoleSystem and see if user wants anything else
+	}
+	return nil
 }
