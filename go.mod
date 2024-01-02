25226c76f0a4b2ea3774794430adaba56cb3cdb8
Dan Gurney <dan@dangurney.net>
2024-01-02 19:58:59 +0000

    feat: set up openai client

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dgurns/clix
 
 go 1.21
+
+require github.com/sashabaranov/go-openai v1.17.9 // indirect
--- internal/llm/openai.go
+++ internal/llm/openai.go
@@ -1,19 +1,46 @@
 package llm
 
+import (
+	"context"
+	"fmt"
+
+	openai "github.com/sashabaranov/go-openai"
+)
+
 type OpenAiLLM struct {
-	OpenAiAPIKey string
+	client *openai.Client
 }
 
 var _ LLM = OpenAiLLM{}
 
 func NewOpenAi(apiKey string) (*OpenAiLLM, error) {
+	c := openai.NewClient(apiKey)
 	return &OpenAiLLM{
-		OpenAiAPIKey: apiKey,
+		client: c,
 	}, nil
 }
 
-func (llm OpenAiLLM) CreateChatCompletion(msgs []*Message) (*Message, error) {
-	// TODO: actually call the OpenAI API
+func (l OpenAiLLM) CreateChatCompletion(msgs []*Message) (*Message, error) {
+	resp, err := l.client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello!",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return nil, err
+	}
+
+	fmt.Println("MESSAGE", resp.Choices[0].Message.Content)
+
 	return &Message{
 		Role:    RoleTool,
 		Content: "ls",
